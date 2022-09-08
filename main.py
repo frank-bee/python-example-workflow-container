d2f57a5e9063453bc23a38738437c3c34a0beda5
Bernhardt Frank (GS/PJ-TOP98) <Frank.Bernhardt@de.bosch.com>
2022-09-08 17:13:46 +0200

    fix

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ import sys
 print(len(sys.argv))
 print(str(sys.argv))
 
-encoder = DataMatrixEncoder('This is a DataMatrix.')
-encoder.save('./datamatrix_test.png')
-print(encoder.get_ascii())
+match sys.argv[1]:
+    case "1":
+        encoder = DataMatrixEncoder('This is a DataMatrix.')
+        encoder.save('./datamatrix_test.png')
+        print(encoder.get_ascii())
+    case "2":
+        print("!yes, this is a 2!!")
+    case "3":
+        print("!yes, this is a 3!!")
+
+    # If an exact match is not confirmed, this last case will be used if provided
+    case _:
+        print("Something's wrong with the internet")
+
+
