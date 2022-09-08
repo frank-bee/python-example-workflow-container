23f8f799f36c87c87c07a8f33b5059f794072dee
Bernhardt Frank (GS/PJ-TOP98) <Frank.Bernhardt@de.bosch.com>
2022-09-08 16:40:53 +0200

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,7 @@
-FROM python:3-slim AS builder
-ADD . /app
-WORKDIR /app
+FROM python:3
 
-# We are installing a dependency here directly into our app source dir
-RUN pip install --target=/app requests
+ADD main.py /
 
-# A distroless container image with Python and some basics like SSL certificates
-# https://github.com/GoogleContainerTools/distroless
-FROM gcr.io/distroless/python3-debian10
-COPY --from=builder /app /app
-WORKDIR /app
-ENV PYTHONPATH /app
-CMD ["/app/main.py"]
+RUN pip install pystrich
+
+CMD [ "python", "./main.py" ]
--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-import os
-import requests  # noqa We are just importing this to prove the dependency installed correctly
+# Sample taken from pyStrich GitHub repository
+# https://github.com/mmulqueen/pyStrich
+from pystrich.datamatrix import DataMatrixEncoder
+import sys
 
+print(len(sys.argv))
+print(str(sys.argv))
 
-def main():
-    my_input = os.environ["INPUT_MYINPUT"]
-
-    my_output = f"Hello {my_input}"
-
-    print(f"::set-output name=myOutput::{my_output}")
-
-
-if __name__ == "__main__":
-    main()
+encoder = DataMatrixEncoder('This is a DataMatrix.')
+encoder.save('./datamatrix_test.png')
+print(encoder.get_ascii())
